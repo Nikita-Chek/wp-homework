1a7d1fe580251f600bb90961978be0df57a92f9f
Nikita <nnikitos0901@gmail.com>
2020-05-27 13:46:42 +0300

    Updates

---
+++ precache-manifest.8ea1748f2d0a422ad23c815d417e3c16.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a696ad2a33ca45bfc28ce399bb8a8f6",
+    "url": "/wp-homework/index.html"
+  },
+  {
+    "revision": "b2f5de1c73edf3f377a6",
+    "url": "/wp-homework/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "aaa74fb20eccc0be75b8",
+    "url": "/wp-homework/static/css/main.2c192f8f.chunk.css"
+  },
+  {
+    "revision": "b2f5de1c73edf3f377a6",
+    "url": "/wp-homework/static/js/2.4c1aa018.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wp-homework/static/js/2.4c1aa018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa74fb20eccc0be75b8",
+    "url": "/wp-homework/static/js/main.5cad5cd6.chunk.js"
+  },
+  {
+    "revision": "566836b061ac81724e17",
+    "url": "/wp-homework/static/js/runtime-main.d4d94e8e.js"
+  },
+  {
+    "revision": "f5012122fabf89f029af48ef6714a6ce",
+    "url": "/wp-homework/static/media/aditional.f5012122.png"
+  },
+  {
+    "revision": "b6d22a271e0059a64fe22085d821c33f",
+    "url": "/wp-homework/static/media/bsu.b6d22a27.png"
+  },
+  {
+    "revision": "f4b5d02dcce52b13e8ef9731f5cc2d17",
+    "url": "/wp-homework/static/media/email.f4b5d02d.png"
+  },
+  {
+    "revision": "ab4e82ef8115e01ef19ed9979eafc2e2",
+    "url": "/wp-homework/static/media/location.ab4e82ef.png"
+  },
+  {
+    "revision": "b7dd8d74da7c9bf6e75c7a3c7604170a",
+    "url": "/wp-homework/static/media/logo.b7dd8d74.png"
+  },
+  {
+    "revision": "fded134bdc500a7a160a9e457296a65d",
+    "url": "/wp-homework/static/media/phone.fded134b.png"
+  },
+  {
+    "revision": "8b7d605316bd77f3f29cb1f16e1d9d8b",
+    "url": "/wp-homework/static/media/search.8b7d6053.png"
+  }
+]);
